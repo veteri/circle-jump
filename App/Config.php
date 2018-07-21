056912001e57dd13965cf9df5aa833d177308461
Dominique <dominique.narbel@gmail.com>
2018-07-21 10:10:43 +0200

    Added config

---
+++ App/Config.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Midi
+ * Date: 18.07.2018
+ * Time: 11:19
+ */
+
+namespace App;
+
+class Config {
+
+    /**
+     * Database host
+     * @var string
+     */
+    const DB_HOST = "your-db-host";
+
+    /**
+     * Database name
+     * @var string
+     */
+    const DB_NAME = "your-db-name";
+
+    /**
+     * Database user
+     * @var string
+     */
+    const DB_USER = "your-db-user";
+
+    /**
+     * Database password
+     * @var string
+     */
+    const DB_PASSWORD = 'your-db-password';
+
+    /**
+     * Show or hide error messages
+     * @var boolean
+     */
+
+    const SHOW_ERRORS = false;
+}
